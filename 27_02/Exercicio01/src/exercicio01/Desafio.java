819ab01f483f24073d59d01feef000f1f6c9e0b8
MatheusMax0105 <152074557+MatheusMax0105@users.noreply.github.com>
2024-02-27 21:36:08 -0300

    Add files via upload
    legal

---
+++ 27_02/Exercicio01/src/exercicio01/Desafio.java
@@ -0,0 +1,11 @@
+package exercicio01;
+import java.util.Scanner;
+public class Desafio {
+    public static void main(String[] args) {
+       int n;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("informe um numero: ");
+             n = entrada.nextInt();
+    }
+}
---
+++ 27_02/Exercicio01/src/exercicio01/Exercicio01.java
@@ -0,0 +1,19 @@
+package exercicio01;
+public class Exercicio01 {
+    public static void main(String[] args) {
+     
+        double a,b,c,d,soma,media;        
+         a = 8;
+         b = 7.5;             
+         c = 4.5;      
+         d = 9;
+        
+        soma = a+b+c+d; 
+        
+        media = soma/4;
+        
+        System.out.println("Media: "+ Double.toString(media));   
+        
+             
+    }
+}
